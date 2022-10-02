5635fdcc5023213502935c83ae3a5d8a976d4a7f
MSGSURYABOT18 <114863977+MSGSURYABOT18@users.noreply.github.com>
2022-10-02 21:26:55 +0530

    Add files via upload

---
+++ tobrot/__init__.py
@@ -0,0 +1,195 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# (c) Shrimadhav U K | gautamajay52 | Amirul Andalib
+
+import asyncio
+import logging
+import os
+import time
+from collections import defaultdict
+from logging.handlers import RotatingFileHandler
+from sys import exit
+import urllib.request
+import dotenv
+import telegram.ext as tg
+
+from pyrogram import Client
+
+if os.path.exists("TorrentLeech-Gdrive.txt"):
+    with open("Torrentleech-Gdrive.txt", "r+") as f_d:
+        f_d.truncate(0)
+
+# the logging things
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(message)s [%(filename)s:%(lineno)d]",
+    datefmt="%d-%b-%y %H:%M:%S",
+    handlers=[
+        RotatingFileHandler(
+            "Torrentleech-Gdrive.txt", maxBytes=50000000, backupCount=10
+        ),
+        logging.StreamHandler(),
+    ],
+)
+logging.getLogger("pyrogram").setLevel(logging.ERROR)
+logging.getLogger("urllib3").setLevel(logging.WARNING)
+logging.getLogger("PIL").setLevel(logging.WARNING)
+
+LOGGER = logging.getLogger(__name__)
+
+user_specific_config=dict()
+
+dotenv.load_dotenv("config.env")
+
+# checking compulsory variable
+for imp in ["TG_BOT_TOKEN", "APP_ID", "API_HASH", "OWNER_ID", "AUTH_CHANNEL"]:
+    try:
+        value = os.environ[imp]
+        if not value:
+            raise KeyError
+    except KeyError:
+        LOGGER.critical(f"Oh...{imp} is missing from config.env ... fill that")
+        exit()
+
+# The Telegram API things
+TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
+# Get these values from my.telegram.org
+APP_ID = int(os.environ.get("APP_ID", 12345))
+API_HASH = os.environ.get("API_HASH")
+
+OWNER_ID = int(os.environ.get("OWNER_ID", 539295917))
+
+# to store the channel ID where bot is authorized
+AUTH_CHANNEL = [int(x) for x in os.environ.get("AUTH_CHANNEL", "539295917").split(' ')]
+# Cuz most ppl dunno AUTH_CHANNEL also works as SUDO
+SUDO_USERS = [int(s) for s in os.environ.get('SUDO_USERS', '').split()]
+AUTH_CHANNEL.append(OWNER_ID)
+AUTH_CHANNEL += SUDO_USERS
+
+# set timeout for subprocess
+PROCESS_MAX_TIMEOUT = 3600
+SP_LIT_ALGO_RITH_M = os.environ.get("SP_LIT_ALGO_RITH_M", "hjs")
+# add offensive API
+TG_OFFENSIVE_API = os.environ.get("TG_OFFENSIVE_API", None)
+INDEX_LINK = os.environ.get("INDEX_LINK", "")
+#################### Rclone vars ##########################
+RCLONE_CONFIG = os.environ.get("RCLONE_CONFIG", "")
+DESTINATION_FOLDER = os.environ.get("DESTINATION_FOLDER", "TorrentLeechX")
+
+#################### Telegram Settings ####################
+MAX_FILE_SIZE = 50000000 # Maximum file size to download from direct links/torrents
+FREE_USER_MAX_FILE_SIZE = 50000000
+TG_MAX_FILE_SIZE = 2097152000 # Telegram maximum file upload size
+MAX_TG_SPLIT_FILE_SIZE = int(os.environ.get("MAX_TG_SPLIT_FILE_SIZE", 1072864000))
+MAX_MESSAGE_LENGTH = 4096 # maximum message length in Telegram
+# add config vars for the display progress
+FINISHED_PROGRESS_STR = os.environ.get("FINISHED_PROGRESS_STR", "█")
+UN_FINISHED_PROGRESS_STR = os.environ.get("UN_FINISHED_PROGRESS_STR", "░")
+# default thumbnail to be used in the videos
+DEF_THUMB_NAIL_VID_S = os.environ.get("DEF_THUMB_NAIL_VID_S", "https://via.placeholder.com/90.jpg")
+EDIT_SLEEP_TIME_OUT = int(os.environ.get("EDIT_SLEEP_TIME_OUT", 15))
+CUSTOM_FILE_NAME = os.environ.get("CUSTOM_FILE_NAME", "") 
+UPLOAD_AS_DOC = os.environ.get("UPLOAD_AS_DOC", "False")
+CUSTOM_FILE_CAPTION = os.environ.get('CUSTOM_FILE_CAPTION', '<code>{file_name}</code>')
+
+################ Torrent/Aria2 Settings ################
+MAX_TIME_TO_WAIT_FOR_TORRENTS_TO_START = int(os.environ.get("MAX_TIME_TO_WAIT_FOR_TORRENTS_TO_START", 600))
+# the download location, where the HTTP Server runs
+DOWNLOAD_LOCATION = "./DOWNLOADS"
+# chunk size that should be used with requests
+CHUNK_SIZE = int(os.environ.get("CHUNK_SIZE", 128))
+ARIA_TWO_STARTED_PORT = int(os.environ.get("ARIA_TWO_STARTED_PORT", 6800))
+
+#################### COMMANDS ####################
+LEECH_COMMAND = os.environ.get("LEECH_COMMAND", "leech")
+LEECH_UNZIP_COMMAND = os.environ.get("LEECH_UNZIP_COMMAND", "extract")
+LEECH_ZIP_COMMAND = os.environ.get("LEECH_ZIP_COMMAND", "archive")
+GLEECH_COMMAND = os.environ.get("GLEECH_COMMAND", "gleech")
+GLEECH_UNZIP_COMMAND = os.environ.get("GLEECH_UNZIP_COMMAND", "gextract")
+GLEECH_ZIP_COMMAND = os.environ.get("GLEECH_ZIP_COMMAND", "garchive")
+YTDL_COMMAND = os.environ.get("YTDL_COMMAND", "ytdl")
+GYTDL_COMMAND = os.environ.get("GYTDL_COMMAND", "gytdl")
+TELEGRAM_LEECH_COMMAND = os.environ.get("TELEGRAM_LEECH_COMMAND", "tleech")
+TELEGRAM_LEECH_UNZIP_COMMAND = os.environ.get("TELEGRAM_LEECH_UNZIP_COMMAND", "tleechextract")
+CANCEL_COMMAND_G = os.environ.get("CANCEL_COMMAND_G", "cancel")
+GET_SIZE_G = os.environ.get("GET_SIZE_G", "getsize")
+STATUS_COMMAND = os.environ.get("STATUS_COMMAND", "status")
+SAVE_THUMBNAIL = os.environ.get("SAVE_THUMBNAIL", "savethumb")
+CLEAR_THUMBNAIL = os.environ.get("CLEAR_THUMBNAIL", "clearthumb")
+PYTDL_COMMAND = os.environ.get("PYTDL_COMMAND", "pytdl")
+GPYTDL_COMMAND = os.environ.get("GPYTDL_COMMAND", "gpytdl")
+LOG_COMMAND = os.environ.get("LOG_COMMAND", "log")
+CLONE_COMMAND_G = os.environ.get("CLONE_COMMAND_G", "gclone")
+UPLOAD_COMMAND = os.environ.get("UPLOAD_COMMAND", "upload")
+RENEWME_COMMAND = os.environ.get("RENEWME_COMMAND", "renewme")
+RENAME_COMMAND = os.environ.get("RENAME_COMMAND", "rename")
+TOGGLE_VID = os.environ.get("TOGGLE_VID", "togglevid")
+TOGGLE_DOC = os.environ.get("TOGGLE_DOC", "toggledoc")
+RCLONE_COMMAND = os.environ.get("RCLONE_COMMAND", "rclone")
+HELP_COMMAND = os.environ.get("HELP_COMMAND", "help")
+SPEEDTEST = os.environ.get("SPEEDTEST", "speedtest")
+TSEARCH_COMMAND = os.environ.get("TSEARCH_COMMAND", "tshelp")
+MEDIAINFO_COMMAND = os.environ.get("MEDIAINFO_COMMAND", "mediainfo")
+TG_DL_COMMAND = os.environ.get("TG_DL_COMMAND", "tgdl")
+MANNUAL_GUP_COMMAND = os.environ.get("MANNUAL_GUP_COMMAND", "gupload") 
+################################################
+
+BOT_START_TIME = time.time()
+
+########## GDTOT/APPDRIVE VARS ##########
+APPDRIVE_EMAIL = os.environ.get('APPDRIVE_EMAIL')
+APPDRIVE_PASS = os.environ.get('APPDRIVE_PASS')
+APPDRIVE_SHARED_DRIVE_ID = os.environ.get('APPDRIVE_SHARED_DRIVE_ID')
+APPDRIVE_FOLDER_ID = os.environ.get('APPDRIVE_FOLDER_ID')
+GDTOT_CRYPT = os.environ.get('GDTOT_CRYPT')
+
+ga_vars_list = ['APPDRIVE_EMAIL', 'APPDRIVE_PASS', 'GDTOT_CRYPT', 'APPDRIVE_SHARED_DRIVE_ID', 'APPDRIVE_FOLDER_ID' ]
+
+for i in ga_vars_list:
+    try:
+        value = os.environ[i]
+        if not value:
+            raise KeyError
+    except KeyError:
+        LOGGER.warning(f"{i} is not provided!! The respective gdtot/appdrive bypass will not work!!!")
+#########################################
+
+# dict to control uploading and downloading
+gDict = defaultdict(lambda: [])
+# user settings dict #ToDo
+user_settings = defaultdict(lambda: {})
+gid_dict = defaultdict(lambda: [])
+_lock = asyncio.Lock()
+
+# Rclone Config Via any raw url
+###########################################################################
+try:                                                                      #
+    RCLONE_CONF_URL = os.environ.get('RCLONE_CONF_URL', "")               #
+    if len(RCLONE_CONF_URL) == 0:                                         #
+        RCLONE_CONF_URL = None                                            #
+    else:                                                                 #
+        urllib.request.urlretrieve(RCLONE_CONF_URL, '/app/rclone.conf')   #
+except KeyError:                                                          #
+    RCLONE_CONF_URL = None                                                #
+###########################################################################
+
+def multi_rclone_init():
+    if RCLONE_CONFIG:
+        LOGGER.warning("Don't use this var now, put your rclone.conf in root directory")
+    if not os.path.exists("rclone.conf"):
+        LOGGER.warning("Sed, No rclone.conf found in root directory")
+        return
+    if not os.path.exists("rclone_bak.conf"):  # backup rclone.conf file
+        with open("rclone_bak.conf", "w+", newline="\n", encoding="utf-8") as fole:
+            with open("rclone.conf", "r") as f:
+                fole.write(f.read())
+        LOGGER.info("rclone.conf backuped to rclone_bak.conf!")
+
+
+multi_rclone_init()
+
+app = Client("LeechBot", bot_token=TG_BOT_TOKEN, api_id=APP_ID, api_hash=API_HASH, workers=343)
+
+updater = tg.Updater(token=TG_BOT_TOKEN)
+bot = updater.bot
+dispatcher = updater.dispatcher
